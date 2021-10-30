57ee06f90139452aeb6a773c7f7c7771d898e4f7
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-10-30 10:01:43 +0700

    #10 Define sale metadata

---
+++ app/src/main/java/com/linh/titledeed/domain/entity/Sale.kt
@@ -0,0 +1,9 @@
+package com.linh.titledeed.domain.entity
+
+data class Sale(
+    val tokenId: String,
+    val title: String,
+    val description: String,
+    val phoneNumber: String,
+    val imageUrls: List<String>
+)
