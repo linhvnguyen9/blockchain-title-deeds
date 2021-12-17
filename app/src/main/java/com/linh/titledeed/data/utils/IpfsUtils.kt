e8b2da6df580270392cd5a104d7a1a510c31a911
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-12-17 21:22:30 +0700

    #32 Change to local IPFS gateway

--- app/src/main/java/com/linh/titledeed/data/utils/IpfsUtils.kt
+++ app/src/main/java/com/linh/titledeed/data/utils/IpfsUtils.kt
@@ -2,5 +2,5 @@ package com.linh.titledeed.data.utils
 
 fun getHttpLinkFromIpfsUri(ipfsUri: String): String {
     val cid = ipfsUri.replace("ipfs://", "")
-    return "https://ipfs.io/ipfs/$cid"
+    return "http://192.168.1.109:8080/ipfs/$cid"
 }
